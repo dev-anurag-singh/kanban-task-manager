f2837f8c3c26874a7503da3c0f37e4a8f8eb7aea
Anurag <dev.anuragkr@gmail.com>
2023-11-12 20:49:49 +0545

    tailwind color config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,5 +8,28 @@ const config: Config = {
   ],
   theme: {},
   plugins: [],
+  colors: {
+    purple: {
+      dark: '#635FC7',
+      light: '#A8A4FF',
+    },
+    black: '#000112',
+    grey: {
+      darkest: '#20212c',
+      dark: '#2B2B37',
+      medium: '#828FA3',
+      light: '#FAF7FD',
+    },
+    lines: {
+      light: '#E4EBFA',
+      dark: '#3E3F4E',
+    },
+    white: '#FFFFFF',
+    transparent: 'transparent',
+    red: {
+      dark: '#EA5555',
+      light: '#FF9898',
+    },
+  },
 };
 export default config;
