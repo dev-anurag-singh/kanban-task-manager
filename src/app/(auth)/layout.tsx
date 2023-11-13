fbb73bbcfc77971c08297787e30fe64b0cd0a472
Anurag <dev.anuragkr@gmail.com>
2023-11-13 17:32:15 +0545

    modified

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { LogoFull } from '@/components/Logo';
 
 function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='p-8 flex flex-col min-h-screen md:items-center md:justify-center'>
+    <div className='p-8 flex flex-col md:min-h-screen md:items-center md:justify-center'>
       <div className='flex flex-col gap-16 md:items-center'>
         <div>
           <LogoFull />
