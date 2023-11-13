3b4b609e96dc2f7733bddd3dd1df222eef437fed
Anurag <dev.anuragkr@gmail.com>
2023-11-13 17:32:01 +0545

    layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={font.className}>{children}</body>
+      <body className={`${font.className}`}>{children}</body>
     </html>
   );
 }
