d07f64abaf67412df230ff45b1a37523ad9c407d
Anurag <dev.anuragkr@gmail.com>
2023-11-13 13:40:29 +0545

    fonts config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,30 +6,38 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
-  plugins: [],
-  colors: {
-    purple: {
-      dark: '#635FC7',
-      light: '#A8A4FF',
+  theme: {
+    colors: {
+      purple: {
+        dark: '#635FC7',
+        light: '#A8A4FF',
+      },
+      black: '#000112',
+      grey: {
+        darkest: '#20212c',
+        dark: '#2B2B37',
+        medium: '#828FA3',
+        light: '#FAF7FD',
+      },
+      lines: {
+        light: '#E4EBFA',
+        dark: '#3E3F4E',
+      },
+      white: '#FFFFFF',
+      transparent: 'transparent',
+      red: {
+        dark: '#EA5555',
+        light: '#FF9898',
+      },
     },
-    black: '#000112',
-    grey: {
-      darkest: '#20212c',
-      dark: '#2B2B37',
-      medium: '#828FA3',
-      light: '#FAF7FD',
-    },
-    lines: {
-      light: '#E4EBFA',
-      dark: '#3E3F4E',
-    },
-    white: '#FFFFFF',
-    transparent: 'transparent',
-    red: {
-      dark: '#EA5555',
-      light: '#FF9898',
+    fontSize: {
+      base:'[2rem,1.4375rem]',
+      xl: '[1.5rem,1.25em]',
+      lg: '[1.125rem,1.25em]',
+      md: '[0.9375rem,1.25em]',
+      sm: '[0.75rem,1.25em]',
     },
   },
+  plugins: [],
 };
 export default config;
