f79c4f3a9068edd402bf609d2d1d9df0b9719896
Anurag <dev.anuragkr@gmail.com>
2023-11-24 23:47:25 +0530

    added svgr

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ];
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
