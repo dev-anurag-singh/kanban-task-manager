272d1761a47765e91752425da3a968e55d43e7df
Anurag <dev.anuragkr@gmail.com>
2023-11-29 01:20:09 +0530

    removed theme.js

--- public/theme.js
+++
@@ -1,7 +0,0 @@
-const theme = window.localStorage.getItem("theme");
-
-if (theme === "dark") {
-  document.documentElement.classList.add("dark");
-} else {
-  document.documentElement.classList.remove("dark");
-}
