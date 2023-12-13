fbf5c87e6c8ac109c5d7dc8e39e05656fb6e4f53
Anurag <dev.anuragkr@gmail.com>
2023-12-13 23:52:01 +0530

    changed breakpoint

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,8 +2,8 @@ import { LogoFull } from "@/components/Logo";
 
 async function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-full flex-col justify-center p-6 md:items-center md:p-8 ">
-      <div className="flex flex-col gap-10 md:w-[30rem] md:gap-16">
+    <div className="flex h-full flex-col justify-center p-6 sm:items-center md:p-8 ">
+      <div className="flex flex-col gap-10 sm:w-[30rem] md:gap-16">
         <div className="flex justify-center">
           <LogoFull />
         </div>
