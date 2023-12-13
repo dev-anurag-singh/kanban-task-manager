2465cd7cc0cb2a943bfb90759a02dfb4f1798586
Anurag <dev.anuragkr@gmail.com>
2023-12-13 23:46:06 +0530

    added shadcn components

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: string;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, error, type, ...props }, ref) => {
+    return (
+      <div className="relative">
+        <input
+          type={type}
+          className={cn(
+            "border-input/25 flex h-10 w-full rounded-md border px-4 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:border-primary focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+            className,
+            {
+              "border-destructive": error,
+            },
+          )}
+          ref={ref}
+          {...props}
+        />
+        {error && (
+          <div
+            aria-live="polite"
+            className="absolute bottom-2 right-2 top-2 bg-muted  px-2 text-base text-destructive"
+          >
+            {error}
+          </div>
+        )}
+      </div>
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
