9f32a39bf406e8c89c8ee28bb1b9a3f7edfa01e4
Anurag <dev.anuragkr@gmail.com>
2023-12-14 18:57:52 +0530

    deleted

--- src/app/app/[board]/loading.tsx
+++
@@ -1,9 +0,0 @@
-function BoardLoading() {
-  return (
-    <div className="grid place-items-center text-xl text-foreground ">
-      Loading ...
-    </div>
-  );
-}
-
-export default BoardLoading;
--- src/app/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-function AppLoading() {
-  return (
-    <div className="grid place-content-center text-xl text-foreground">
-      Loading...
-    </div>
-  );
-}
-
-export default AppLoading;
