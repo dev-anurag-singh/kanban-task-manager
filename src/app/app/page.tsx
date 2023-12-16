9851d6e6f2fd2664175d8a78eb25584ad36fd7e7
Anurag <dev.anuragkr@gmail.com>
2023-12-16 23:51:23 +0530

    async

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,8 +1,8 @@
-function Home() {
+async function Home() {
   return (
     <div className="grid basis-full place-items-center">
       <div className=" max-w-[21rem] space-y-6 text-center">
-        <p className="text-muted-foreground text-lg">
+        <p className="text-lg text-muted-foreground">
           This page will be implemented at a later date.
         </p>
       </div>
