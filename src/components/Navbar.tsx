87f5d522af733d5218e76d16e99b8ee6d0b27aea
Anurag <dev.anuragkr@gmail.com>
2023-12-18 00:24:31 +0530

    added Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,36 @@
-// import EllipsisVertical from "@/icons/EllipsisVertical.svg";
-// import IconPlus from "@/icons/IconPlus.svg";
-// import Button from "./Button";
+import EllipsisVertical from "@/icons/EllipsisVertical.svg";
+import IconPlus from "@/icons/IconPlus.svg";
+import { Button } from "./ui/button";
 
-// function Navbar({
-//   board,
-// }: {
-//   board: {
-//     name: string;
-//   };
-// }) {
-//   return (
-//     <>
-//       <div className="flex">
-//         <div className="flex grow items-center gap-4 p-4 md:px-6 ">
-//           <div className="mr-auto flex">
-//             <h4 className="text-lg">{board.name}</h4>
-//           </div>
-//           <div className="hidden md:block">
-//             <Button disabled={true} size="large">
-//               + Add New Task
-//             </Button>
-//           </div>
-//           <div className="rounded-3xl shadow-lg md:hidden">
-//             <Button disabled={true} size="medium">
-//               <IconPlus />
-//             </Button>
-//           </div>
-//           <button>
-//             <EllipsisVertical />
-//           </button>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+function Navbar({
+  board,
+}: {
+  board: {
+    title: string;
+  };
+}) {
+  return (
+    <>
+      <div className="flex bg-muted border-b">
+        <div className="flex grow items-center p-4 md:px-6 ">
+          <div className="mr-auto flex">
+            <h4 className="text-lg">{board.title}</h4>
+          </div>
+          <div className="hidden md:block">
+            <Button size="lg">+ Add New Task</Button>
+          </div>
+          <div className="rounded-3xl shadow-lg md:hidden">
+            <Button size="icon">
+              <IconPlus />
+            </Button>
+          </div>
+          <Button size="icon" variant="ghost" className="ml-1">
+            <EllipsisVertical />
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
 
-// export default Navbar;
+export default Navbar;
