13720903e23c12d7218dbc6cf38c6853e6b16859
Anurag <dev.anuragkr@gmail.com>
2023-12-18 00:24:15 +0530

    updated loading page with skeleton

--- src/app/app/[boardId]/loading.tsx
+++ src/app/app/[boardId]/loading.tsx
@@ -1,5 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="grid grid-cols-[repeat(3,17.5rem)] gap-6 overflow-hidden p-6">
+      <Skeleton className="w-72" />
+
+      <Skeleton className="w-72" />
+
+      <Skeleton className="w-72" />
+    </div>
+  );
 }
 
 export default Loading;
