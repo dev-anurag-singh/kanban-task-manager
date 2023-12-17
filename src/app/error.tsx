4ca4097f325c8e08d08aabe3e849fcf447cb3962
Anurag <dev.anuragkr@gmail.com>
2023-12-17 18:11:42 +0530

    global error handler

---
+++ src/app/error.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <main className="grid h-full place-items-center">
+      <div className=" max-w-[21rem] space-y-6 text-center">
+        <p className="text-lg text-muted-foreground">{error.message}</p>
+        <Button
+          size={"lg"}
+          onClick={
+            // Attempt to recover by trying to re-render the invoices route
+            () => reset()
+          }
+        >
+          {" "}
+          Try again
+        </Button>
+      </div>
+    </main>
+  );
+}
+
+export default Error;
