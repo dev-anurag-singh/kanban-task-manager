bc5cc41471cc7b7b722e2158dc5d7524eb16d67a
Anurag <dev.anuragkr@gmail.com>
2023-12-18 00:23:49 +0530

    boards page

--- src/app/app/[boardId]/page.tsx
+++ src/app/app/[boardId]/page.tsx
@@ -1,5 +1,19 @@
-function BoardPage() {
-  return <div>Board page</div>;
+import { Button } from "@/components/ui/button";
+
+async function BoardPage() {
+  // just for testing purposes
+  await new Promise((r) => setTimeout(r, 4000));
+
+  return (
+    <main className="grid h-full place-items-center p-6">
+      <div className=" max-w-[21rem] space-y-6 text-center">
+        <p className="text-lg text-muted-foreground">
+          This board is empty. Create a new column to get started.
+        </p>
+        <Button size={"lg"}>+ Add new column</Button>
+      </div>
+    </main>
+  );
 }
 
 export default BoardPage;
