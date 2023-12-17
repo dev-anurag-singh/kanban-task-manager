a4eaf15e4751e888a6a4373a7a488c1bb96a9475
Anurag <dev.anuragkr@gmail.com>
2023-12-18 00:22:15 +0530

    added server action to fetch board by ID

---
+++ src/actions/get-board-by-id.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import supabaseServerClient from "@/lib/supabase/serverClient";
+
+export async function getBoardById(id: string) {
+  const supabase = await supabaseServerClient();
+
+  const { data } = await supabase
+    .from("boards")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  return data;
+}
