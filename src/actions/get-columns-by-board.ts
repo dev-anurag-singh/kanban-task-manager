41950aba90178930d5aa5f1e1d3474eaf7d8d227
Anurag <dev.anuragkr@gmail.com>
2023-12-18 20:38:57 +0530

    added: getColumnsByBoard server action

---
+++ src/actions/get-columns-by-board.ts
@@ -0,0 +1,13 @@
+"use server";
+import supabaseServerClient from "@/lib/supabase/serverClient";
+
+export async function getColumnsByBoardId(id: string) {
+  const supabase = await supabaseServerClient();
+
+  const { data } = await supabase
+    .from("columns")
+    .select(`*,tasks(*)`)
+    .eq("board_id", id);
+
+  return data;
+}
