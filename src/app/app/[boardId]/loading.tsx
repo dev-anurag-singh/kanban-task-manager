e5736438f261981acea6d34a395c152dfd9739c0
Anurag <dev.anuragkr@gmail.com>
2023-12-18 20:43:19 +0530

    updated loading page

--- src/app/app/[boardId]/loading.tsx
+++ src/app/app/[boardId]/loading.tsx
@@ -2,12 +2,37 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 function Loading() {
   return (
-    <div className="grid grid-cols-[repeat(3,17.5rem)] gap-6 overflow-hidden p-6">
-      <Skeleton className="w-72" />
-
-      <Skeleton className="w-72" />
-
-      <Skeleton className="w-72" />
+    <div className="flex gap-6 overflow-x-scroll p-6">
+      <div className="flex flex-col gap-6">
+        <Skeleton className="h-5" />
+        <div className="flex flex-col gap-5">
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-6">
+        <Skeleton className="h-5" />
+        <div className="flex flex-col gap-5">
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-6">
+        <Skeleton className="h-5" />
+        <div className="flex flex-col gap-5">
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+          <Skeleton className="h-[90px] w-72" />
+        </div>
+      </div>
     </div>
   );
 }
