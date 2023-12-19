f70fa03e7619ae959dacd16ccdf1504866c15150
Anurag <dev.anuragkr@gmail.com>
2023-12-19 16:16:43 +0530

    added new types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,6 +3,7 @@ import { Tables } from "./database.types";
 export type Column = Tables<"columns">;
 export type Task = Tables<"tasks">;
 export type Board = Tables<"boards">;
+export type Subtasks = Tables<"subtasks">;
 
-export type ColumnWithTasks = Column & { tasks: Task[] };
-export type ColumnWithTasksId = Column & { tasks: { id: string }[] };
+export type TaskWithSubtasks = Task & { subtasks: Subtasks[] };
+export type BoardWithColumns = Board & { columns: Column[] };
