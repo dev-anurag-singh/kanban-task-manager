95d68d389d951c8ecfeadba2ee18e7986cff2238
Anurag <dev.anuragkr@gmail.com>
2023-12-23 22:54:02 +0530

    modified

--- src/app/app/not-found.tsx
+++ src/app/app/not-found.tsx
@@ -6,7 +6,7 @@ function NotFound() {
     <main className="grid basis-full place-items-center">
       <div className=" max-w-[21rem] space-y-6 text-center">
         <p className="text-lg text-muted-foreground">
-          Could not find the requested resource
+          Could not find the requested board
         </p>
         <Link className={buttonVariants({ variant: "link" })} href="/">
           Return Home
