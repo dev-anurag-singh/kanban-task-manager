7818a4b9d4a693cce677bf02834746b1f84f9ecd
Anurag <dev.anuragkr@gmail.com>
2023-12-23 22:54:14 +0530

    modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,31 +9,29 @@ function Navbar({ board }: { board: BoardWithColumns }) {
   const disabled = board.columns.length === 0;
 
   return (
-    <>
-      <div className="flex border-b bg-muted">
-        <div className="flex grow items-center p-4 md:px-6 ">
-          <Link href="/app" className="mr-4 md:hidden ">
-            <LogoSmall />
-          </Link>
-          <div className="mr-auto flex">
-            <h4 className="text-lg">{board.title}</h4>
-          </div>
-          <div className="hidden md:block">
-            <Button disabled={disabled} size="lg">
-              + Add New Task
-            </Button>
-          </div>
-          <div className="rounded-3xl shadow-lg md:hidden">
-            <Button disabled={disabled} size="icon">
-              <IconPlus />
-            </Button>
-          </div>
-          <Button size="icon" variant="ghost" className="ml-1">
-            <EllipsisVertical />
+    <div className="flex border-b bg-muted">
+      <div className="flex grow items-center p-4 md:px-6 ">
+        <Link href="/app" className="mr-4 md:hidden ">
+          <LogoSmall />
+        </Link>
+        <div className="mr-auto flex">
+          <h4 className="text-lg">{board.title}</h4>
+        </div>
+        <div className="hidden md:block">
+          <Button disabled={disabled} size="lg">
+            + Add New Task
           </Button>
         </div>
+        <div className="rounded-3xl shadow-lg md:hidden">
+          <Button disabled={disabled} size="icon">
+            <IconPlus />
+          </Button>
+        </div>
+        <Button size="icon" variant="ghost" className="ml-1">
+          <EllipsisVertical />
+        </Button>
       </div>
-    </>
+    </div>
   );
 }
 
