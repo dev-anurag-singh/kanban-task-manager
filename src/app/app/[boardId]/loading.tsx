26bdbf40c149f499a58b6b89b3405a75a4e5d5dc
Anurag <dev.anuragkr@gmail.com>
2023-12-25 23:52:06 +0530

    made changes to flexbox

--- src/app/app/[boardId]/loading.tsx
+++ src/app/app/[boardId]/loading.tsx
@@ -2,32 +2,26 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 function Loading() {
   return (
-    <div className="flex gap-6 overflow-x-scroll p-6">
-      <div className="flex flex-col gap-6">
+    <div className="flex gap-6 overflow-hidden overflow-x-scroll p-6">
+      <div className="space-y-6">
         <Skeleton className="h-5" />
-        <div className="flex flex-col gap-5">
-          <Skeleton className="h-[90px] w-72" />
-          <Skeleton className="h-[90px] w-72" />
+        <div className="space-y-5">
           <Skeleton className="h-[90px] w-72" />
           <Skeleton className="h-[90px] w-72" />
           <Skeleton className="h-[90px] w-72" />
         </div>
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="space-y-6">
         <Skeleton className="h-5" />
-        <div className="flex flex-col gap-5">
-          <Skeleton className="h-[90px] w-72" />
-          <Skeleton className="h-[90px] w-72" />
+        <div className="space-y-5">
           <Skeleton className="h-[90px] w-72" />
           <Skeleton className="h-[90px] w-72" />
           <Skeleton className="h-[90px] w-72" />
         </div>
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="space-y-6">
         <Skeleton className="h-5" />
-        <div className="flex flex-col gap-5">
-          <Skeleton className="h-[90px] w-72" />
-          <Skeleton className="h-[90px] w-72" />
+        <div className="space-y-5">
           <Skeleton className="h-[90px] w-72" />
           <Skeleton className="h-[90px] w-72" />
           <Skeleton className="h-[90px] w-72" />
