1145b6c95e64d22ac1168c0bc334bed8f7f0fe79
Anurag <dev.anuragkr@gmail.com>
2023-12-25 23:31:36 +0530

    added new bg-gradient

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage:{
+        skeleton: "linear-gradient(var(--skeleton))",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -50,6 +53,7 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        
       },
       borderRadius: {
         lg: "var(--radius)",
