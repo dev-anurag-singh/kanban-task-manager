2ff1a1c5c00f9a9eaea277807b81b3ee2ccae107
Anurag <dev.anuragkr@gmail.com>
2023-12-27 19:22:10 +0530

    created sidebar dropdown for mobile navigation

---
+++ src/components/Sidebar/SidebarDropDown.tsx
@@ -0,0 +1,26 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
+import NavItems from "./NavItems";
+import ThemeToggler from "./ThemeToggler";
+
+function SidebarDropDown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="transition-transform data-[state=open]:rotate-180">
+        <ChevronDown />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent sideOffset={30}>
+        <div className="flex w-64 flex-col gap-5 py-4">
+          <NavItems />
+          <ThemeToggler />
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default SidebarDropDown;
