5a6e290656417ebd49ebd7f5a6c414d591df935b
Anurag <dev.anuragkr@gmail.com>
2023-12-27 19:22:57 +0530

    updated

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,8 +3,6 @@ import { Tables } from "./database.types";
 export type Column = Tables<"columns">;
 export type Task = Tables<"tasks">;
 export type Board = Tables<"boards">;
-export type Subtasks = Tables<"subtasks">;
 
 export type ColumnWithTasks = Column & { tasks: Task[] };
-export type TaskWithSubtasks = Task & { subtasks: Subtasks[] };
 export type BoardWithColumns = Board & { columns: Column[] };
