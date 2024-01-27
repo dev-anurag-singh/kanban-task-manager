3f5477f93f3570e7979849c4219ca28b947410f8
Anurag <dev.anuragkr@gmail.com>
2024-01-27 21:44:05 +0530

    added redirection

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,6 +1,15 @@
+import { getAllBoards } from "@/actions/get-all-boards";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
-function Page() {
+async function Page() {
+  const boards = await getAllBoards();
+
+  if (!!boards) {
+    const redirectTo = boards.at(0);
+    redirect(`/app/${redirectTo?.id}`);
+  }
+
   return (
     <main className="grid h-full place-items-center">
       <div className=" max-w-[21rem] space-y-6 text-center">
