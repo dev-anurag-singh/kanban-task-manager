b6857032c364187cdb445670ffe91e65f302052a
Anurag <dev.anuragkr@gmail.com>
2024-01-30 03:02:52 +0530

    Added drag and drop for columns

--- src/components/Board/Column.tsx
+++ src/components/Board/Column.tsx
@@ -1,12 +1,36 @@
+"use client";
 import type { ColumnWithTasks } from "@/lib/types";
 import Task from "./Task";
 import { ScrollArea } from "../ui/scroll-area";
+import { cn } from "@/lib/utils";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface ColumnProps {
   column: ColumnWithTasks;
 }
 
-async function Column({ column: { id, title, tasks } }: ColumnProps) {
+function Column({ column: { id, title, tasks, order } }: ColumnProps) {
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id,
+    data: {
+      type: "Column",
+      id,
+      title,
+    },
+  });
+  const style = {
+    transition,
+    transform: CSS.Translate.toString(transform),
+  };
+
   // Tasks which are direct child of a column
   const columnTasks = tasks.filter((task) => !task.parent_task_id);
 
@@ -14,15 +38,24 @@ async function Column({ column: { id, title, tasks } }: ColumnProps) {
 
   return (
     <div
-      key={id}
-      className="relative flex w-72 shrink-0 flex-col gap-6 px-2 py-3"
+      ref={setNodeRef}
+      style={style}
+      className={cn(
+        "relative flex max-h-full w-72 shrink-0 flex-col gap-6 bg-background px-2 py-3",
+        isDragging &&
+          "before:absolute before:inset-0 before:z-10 before:rounded-sm before:bg-muted",
+      )}
     >
-      <div className="list-border space-x-2 text-md uppercase text-muted-foreground hover:before:border-border">
+      <div
+        {...listeners}
+        {...attributes}
+        className="list-border cursor-grab space-x-2 text-md uppercase text-muted-foreground hover:before:border-border active:before:border-border aria-pressed:cursor-grabbing"
+      >
         <span>{title}</span>
         <span>({tasksCount})</span>
       </div>
-      <ScrollArea className="basis-full">
-        <div className="flex flex-col gap-5">
+      <ScrollArea className="flex h-full flex-col">
+        <div className="space-y-5">
           {columnTasks.map((task) => (
             <Task key={task.id} task={task} tasks={tasks} />
           ))}
--- src/components/Board/ColumnContainer.tsx
+++ src/components/Board/ColumnContainer.tsx
@@ -1,9 +1,17 @@
 "use client";
-
-import { cn } from "@/lib/utils";
 import Column from "./Column";
 import { ColumnWithTasks } from "@/lib/types";
-import { useState } from "react";
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  closestCenter,
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
+import { useEffect, useMemo, useState } from "react";
+import { createPortal } from "react-dom";
+import { useReorderColumns } from "./useReorderColumns";
 
 interface ColumnContainerProps {
   columns: ColumnWithTasks[];
@@ -11,14 +19,74 @@ interface ColumnContainerProps {
 
 function ColumnContainer({ columns }: ColumnContainerProps) {
   const [orderedColumns, setOrderedColumns] = useState(columns);
+  const [mounted, setMounted] = useState(false);
+  const [activeId, setActiveId] = useState<null | string>(null);
+
+  const { reorderColumns, isPending } = useReorderColumns();
+
+  const activeColumn = useMemo(() => {
+    return orderedColumns.find((column) => column.id === activeId) || null;
+  }, [activeId, orderedColumns]);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
-    <main className={cn("flex gap-6")}>
-      {orderedColumns.map((column) => (
-        <Column key={column.id} column={column} />
-      ))}
-    </main>
+    <DndContext
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
+      <div className="flex items-start gap-6">
+        <SortableContext items={orderedColumns}>
+          {orderedColumns.map((column) => (
+            <Column key={column.id} column={column} />
+          ))}
+        </SortableContext>
+      </div>
+
+      {createPortal(
+        <DragOverlay>
+          {activeColumn && <Column column={activeColumn} />}
+        </DragOverlay>,
+        document.body,
+      )}
+    </DndContext>
   );
+
+  function handleDragEnd(event: DragEndEvent) {
+    // When Columns are Dragged
+    const { active, over } = event;
+    if (!over) return;
+
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+
+    if (activeColumnId === overColumnId) return;
+
+    const reorderedColumns = arrayMove(
+      orderedColumns,
+      active.data.current?.sortable.index,
+      over.data.current?.sortable.index,
+    ).map((item, index) => ({ ...item, order: index }));
+
+    setOrderedColumns(reorderedColumns);
+
+    // updating data into the database
+
+    reorderColumns(reorderedColumns);
+  }
+
+  function handleDragStart(event: DragStartEvent) {
+    if (event.active.data.current?.type === "Column") {
+      setActiveId(event.active.data.current.id);
+    }
+  }
 }
 
 export default ColumnContainer;
