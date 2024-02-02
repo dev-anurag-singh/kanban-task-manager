5dead09c465137c669781645e6d2954bddc69f22
Anurag <dev.anuragkr@gmail.com>
2024-02-02 21:59:22 +0530

    kanban

--- src/components/Board/ColumnContainer.tsx
+++ src/components/Board/ColumnContainer.tsx
@@ -1,22 +1,24 @@
 "use client";
 import Column from "./Column";
-import { ColumnWithTasks, TaskWithSubtasks } from "@/lib/types";
+import {
+  ColumnWithTasks,
+  ColumnWithTasksAndSubtasks,
+  TaskWithSubtasks,
+} from "@/lib/types";
 import {
   DndContext,
   DragEndEvent,
   DragOverEvent,
   DragOverlay,
   DragStartEvent,
-  KeyboardSensor,
   PointerSensor,
   closestCenter,
-  closestCorners,
   pointerWithin,
   useSensor,
   useSensors,
 } from "@dnd-kit/core";
 import { SortableContext, arrayMove } from "@dnd-kit/sortable";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import { useReorderColumns } from "./useReorderColumns";
 import Task from "./Task";
@@ -27,19 +29,13 @@ interface ColumnContainerProps {
 }
 
 function ColumnContainer({ columns }: ColumnContainerProps) {
+  const [mounted, setMounted] = useState(false);
   // CUSTOM HOOK TO STRUCTURE TASKS
   const { orderedColumns, setOrderedColumns } = useOrderedColumns(columns);
-
-  const [mounted, setMounted] = useState(false);
-  const [activeColumnId, setActiveColumnId] = useState<null | string>(null);
+  const [activeColumn, setActiveColumn] =
+    useState<null | ColumnWithTasksAndSubtasks>(null);
   const [activeTask, setActiveTask] = useState<TaskWithSubtasks | null>(null);
   const { reorderColumns, isPending } = useReorderColumns();
-
-  const activeColumn = useMemo(() => {
-    return (
-      orderedColumns.find((column) => column.id === activeColumnId) || null
-    );
-  }, [activeColumnId, orderedColumns]);
 
   const sensors = useSensors(
     useSensor(PointerSensor, {
@@ -63,6 +59,7 @@ function ColumnContainer({ columns }: ColumnContainerProps) {
       onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
       sensors={sensors}
+      collisionDetection={closestCenter}
     >
       <div className=" flex gap-6">
         <SortableContext items={orderedColumns}>
@@ -83,7 +80,7 @@ function ColumnContainer({ columns }: ColumnContainerProps) {
   );
 
   function handleDragEnd(event: DragEndEvent) {
-    setActiveColumnId(null);
+    setActiveColumn(null);
     setActiveTask(null);
     // When Columns are Dragged
     const { active, over } = event;
@@ -108,7 +105,7 @@ function ColumnContainer({ columns }: ColumnContainerProps) {
 
   function handleDragStart(event: DragStartEvent) {
     if (event.active.data.current?.type === "Column") {
-      setActiveColumnId(event.active.data.current.id);
+      setActiveColumn(event.active.data.current.column);
     }
 
     if (event.active.data.current?.type === "Task") {
@@ -118,11 +115,105 @@ function ColumnContainer({ columns }: ColumnContainerProps) {
 
   function handleDragOver(event: DragOverEvent) {
     const { active, over } = event;
-    console.log(over);
 
     if (!over) return;
 
     if (active.id === over.id) return;
+
+    const isActiveATask = active.data.current?.type === "Task";
+    const isOverATask = over.data.current?.type === "Task";
+    const isOverAColumn = over.data.current?.type === "Column";
+
+    if (!isActiveATask) return;
+
+    const newOrderedData = [...orderedColumns];
+
+    const activeTaskColumn = active.data.current?.task.column_id;
+
+    let sourceList = newOrderedData.find((col) => col.id === activeTaskColumn);
+
+    if (!sourceList) {
+      return;
+    }
+
+    if (isOverATask) {
+      const overTaskColumn = over.data.current?.task.column_id;
+
+      // If dragged inside the same column
+      if (activeTaskColumn === overTaskColumn) {
+        const reorderedTasks = arrayMove(
+          sourceList.tasks,
+          active.data.current?.sortable.index,
+          over.data.current?.sortable.index,
+        ).map((task, i) => ({ ...task, order: i }));
+
+        sourceList.tasks = reorderedTasks;
+
+        console.log(reorderedTasks);
+
+        setOrderedColumns(newOrderedData);
+      }
+      // If dragged insie another column
+      else {
+        let destList = newOrderedData.find((col) => col.id === overTaskColumn);
+        if (!destList) {
+          return;
+        }
+        const activeIndex = sourceList.tasks.findIndex(
+          (t) => t.id === active.data.current?.task.id,
+        );
+        const overIndex = destList.tasks.findIndex(
+          (t) => t.id === over.data.current?.task.id,
+        );
+
+        // Remove Task form source List
+        const [movedTask] = sourceList.tasks.splice(activeIndex, 1);
+
+        movedTask.column_id = overTaskColumn;
+        movedTask.subtasks.forEach((sub) => {
+          sub.column_id = overTaskColumn;
+        });
+
+        destList.tasks.splice(overIndex, 0, movedTask);
+        sourceList.tasks.forEach((t, i) => {
+          t.order = i;
+        });
+
+        destList.tasks.forEach((t, i) => {
+          t.order = i;
+        });
+
+        setOrderedColumns(newOrderedData);
+      }
+    }
+    if (isOverAColumn) {
+      // If dragged over another column
+
+      if (activeTaskColumn !== over.id) {
+        let destList = newOrderedData.find((col) => col.id === over.id);
+        if (!destList) {
+          return;
+        }
+        const activeIndex = sourceList.tasks.findIndex(
+          (t) => t.id === active.data.current?.task.id,
+        );
+        const overIndex = destList.tasks.length;
+        // Remove Task form source List
+        const [movedTask] = sourceList.tasks.splice(activeIndex, 1);
+        movedTask.column_id = over.id as string;
+        movedTask.subtasks.forEach((sub) => {
+          sub.column_id = over.id as string;
+        });
+        destList.tasks.splice(overIndex, 0, movedTask);
+        sourceList.tasks.forEach((t, i) => {
+          t.order = i;
+        });
+        destList.tasks.forEach((t, i) => {
+          t.order = i;
+        });
+        setOrderedColumns(newOrderedData);
+      }
+    }
   }
 }
 
