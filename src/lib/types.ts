9ad0cedf90515540cb0ca67c7d444cc7e217acb0
Anurag <dev.anuragkr@gmail.com>
2024-02-04 23:46:21 +0530

    create new types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,7 +4,8 @@ export type Column = Tables<"columns">;
 export type Task = Tables<"tasks">;
 export type Board = Tables<"boards">;
 
-export type ColumnWithTasks = Column & { tasks: Task[] };
+export type Columns = Column[];
+export type Tasks = Task[];
+export type TaskAndSubtasks = { task: Task; subtasks: Task[] };
+
 export type BoardWithColumns = Board & { columns: Column[] };
-export type TaskWithSubtasks = Task & { subtasks: Task[] };
-export type ColumnWithTasksAndSubtasks = Column & { tasks: TaskWithSubtasks[] };
