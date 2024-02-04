3fb4efb51dfc3b7106c84ba188b157f9ed1e18d0
Anurag <dev.anuragkr@gmail.com>
2024-02-04 23:47:35 +0530

    remove dnd feature from columns

--- src/components/Board/Column.tsx
+++ src/components/Board/Column.tsx
@@ -1,5 +1,5 @@
 "use client";
-import type { ColumnWithTasksAndSubtasks } from "@/lib/types";
+import type { Column, Tasks } from "@/lib/types";
 import Task from "./Task";
 import { ScrollArea } from "../ui/scroll-area";
 import { cn } from "@/lib/utils";
@@ -8,58 +8,50 @@ import {
   useSortable,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
+import { useMemo, useState } from "react";
 
 interface ColumnProps {
-  column: ColumnWithTasksAndSubtasks;
+  column: Column;
+  tasks: Tasks;
 }
 
-function Column({ column }: ColumnProps) {
-  const { id, title, tasks } = column;
-  const {
-    setNodeRef,
-    attributes,
-    listeners,
-    transform,
-    transition,
-    isDragging,
-  } = useSortable({
-    id,
+function Column({ column: { title, id }, tasks }: ColumnProps) {
+  const { setNodeRef } = useSortable({
+    id: id,
     data: {
       type: "Column",
-      column,
     },
   });
-  const style = {
-    transition,
-    transform: CSS.Translate.toString(transform),
-  };
+  const columnTasks = useMemo(
+    () => tasks.filter((task) => !task.parent_task_id),
+    [tasks],
+  );
 
   const tasksCount = tasks.length;
 
   return (
     <div
       ref={setNodeRef}
-      style={style}
       className={cn(
         "relative flex h-full w-72 shrink-0 flex-col gap-6 bg-background px-2 py-3",
-        isDragging &&
-          "before:absolute before:inset-0 before:z-10 before:rounded-sm before:bg-muted",
       )}
     >
-      <div
-        {...listeners}
-        {...attributes}
-        className="list-border space-x-2 text-md uppercase text-muted-foreground hover:cursor-grab hover:before:border-border active:before:border-border"
-      >
+      <div className="list-border space-x-2 text-md uppercase text-muted-foreground hover:cursor-grab hover:before:border-border active:before:border-border">
         <span>{title}</span>
         <span>({tasksCount})</span>
       </div>
       <ScrollArea className="flex h-full flex-col">
-        <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+        <SortableContext
+          items={columnTasks}
+          strategy={verticalListSortingStrategy}
+        >
           <div className="flex flex-col gap-5 pb-2">
-            {tasks.map((task) => (
-              <Task key={task.id} task={task} />
+            {columnTasks.map((task) => (
+              <Task
+                key={task.id}
+                task={task}
+                subtasks={tasks.filter((t) => t.parent_task_id === task.id)}
+              />
             ))}
           </div>
         </SortableContext>
