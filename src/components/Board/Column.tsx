b397080406fe3e2845462d16b3867a4036717736
Anurag <dev.anuragkr@gmail.com>
2024-02-18 23:24:41 +0530

    added custom color to columns

--- src/components/Board/Column.tsx
+++ src/components/Board/Column.tsx
@@ -1,4 +1,5 @@
 "use client";
+import stc from "string-to-color";
 import type { Column, Tasks } from "@/lib/types";
 import Task from "./Task";
 import { ScrollArea } from "../ui/scroll-area";
@@ -23,6 +24,7 @@ function Column({ column: { title, id }, tasks }: ColumnProps) {
   });
 
   const tasksCount = tasks.length;
+  const columnColor = stc(id || "");
 
   return (
     <div
@@ -31,7 +33,11 @@ function Column({ column: { title, id }, tasks }: ColumnProps) {
         "relative flex h-full w-72 shrink-0 flex-col gap-6 bg-background px-2 py-3",
       )}
     >
-      <div className="list-border space-x-2 text-md uppercase text-muted-foreground hover:cursor-grab hover:before:border-border active:before:border-border">
+      <div className="flex items-center gap-2 text-md uppercase text-muted-foreground">
+        <span
+          style={{ backgroundColor: columnColor }}
+          className="h-4 w-4 rounded-full"
+        />
         <span>{title}</span>
         <span>({tasksCount})</span>
       </div>
