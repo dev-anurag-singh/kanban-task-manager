9496b8070ab9a35c92c2fd504ade3be371b47b81
Anurag <dev.anuragkr@gmail.com>
2024-02-23 23:49:40 +0530

    change boards to title

--- src/lib/validators/board-validator.ts
+++ src/lib/validators/board-validator.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const BoardValidator = z.object({
-  board: z.string().min(4, { message: "Minimun 4 characters" }),
+  title: z.string().min(4, { message: "Minimun 4 characters" }),
   columns: z
     .object({
       column: z.string().min(4, {
