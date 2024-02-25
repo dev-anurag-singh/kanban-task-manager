9a7ebf8f2dbfeaf4cfcd3f1cca4d60da16ee6bd2
Anurag <dev.anuragkr@gmail.com>
2024-02-25 23:19:38 +0530

    update board hook

---
+++ src/components/Board/useUpdateBoard.ts
@@ -0,0 +1,21 @@
+import { updateBoard } from "@/services/apiBoards";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+export function useUpdateBoard() {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const { mutate: editBoard, isPending } = useMutation({
+    mutationFn: updateBoard,
+    onSuccess: (data) => {
+      toast.success("Board updated successfully");
+      queryClient.invalidateQueries({ queryKey: ["boards"] });
+      router.refresh();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { editBoard, isPending };
+}
