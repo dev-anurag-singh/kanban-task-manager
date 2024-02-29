ae420708447cd4d06d6a37a08bce1bd43a8a55a0
Anurag <dev.anuragkr@gmail.com>
2024-02-29 19:35:22 +0530

    modified to allow align prop

--- src/components/Sidebar/SidebarDropDown.tsx
+++ src/components/Sidebar/SidebarDropDown.tsx
@@ -7,13 +7,18 @@ import { ChevronDown } from "lucide-react";
 import NavItems from "./NavItems";
 import ThemeToggler from "./ThemeToggler";
 
-function SidebarDropDown() {
+function SidebarDropDown({ align }: { align?: "start" | "end" }) {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="transition-transform data-[state=open]:rotate-180">
+      <DropdownMenuTrigger className="transition-transform data-[state=open]:rotate-180 md:hidden">
         <ChevronDown className="-mb-0.5 h-5 w-5" />
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="md:hidden" sideOffset={30} side="bottom">
+      <DropdownMenuContent
+        className="text-muted-foreground md:hidden"
+        sideOffset={30}
+        side="bottom"
+        align={align || "start"}
+      >
         <div className="flex w-64 flex-col gap-5 py-4">
           <NavItems />
           <ThemeToggler />
