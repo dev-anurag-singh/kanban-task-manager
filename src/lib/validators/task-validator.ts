83240ecdbfb5a9587a4318bbce9ae29ba4bbd2e9
Anurag <dev.anuragkr@gmail.com>
2024-03-07 20:21:53 +0530

    modified

--- src/lib/validators/task-validator.ts
+++ src/lib/validators/task-validator.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
 
 export const TaskValidator = z.object({
-  title: z.string().min(4, { message: "Minimun 4 characters" }),
+  title: z.string().min(1, { message: "Can't be empty" }),
   subtasks: z
     .object({
       title: z.string().min(1, { message: "Can't be empty" }),
     })
     .array(),
   description: z.string(),
+  column_id: z.number(),
 });
 
 export type TTaskValidator = z.infer<typeof TaskValidator>;
