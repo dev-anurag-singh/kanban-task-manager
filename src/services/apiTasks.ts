8e8970382592238fa72dc55175c8337ff66d77a4
Anurag <dev.anuragkr@gmail.com>
2024-03-07 20:20:55 +0530

    add task api

--- src/services/apiTasks.ts
+++ src/services/apiTasks.ts
@@ -11,3 +11,54 @@ export async function reorderTasks(tasks: Tasks) {
 
   return data;
 }
+
+export async function createTask({
+  title,
+  subtasks,
+  description,
+  column_id,
+  board_id,
+}: {
+  title: string;
+  subtasks: { title: string }[];
+  description: string;
+  column_id: number;
+  board_id: string;
+}) {
+  const { count, error: countError } = await supabaseBrowserClient
+    .from("tasks")
+    .select("*", { count: "exact", head: true })
+    .eq("column_id", column_id);
+
+  if (countError) throw new Error(countError.message);
+
+  const { data, error } = await supabaseBrowserClient
+    .from("tasks")
+    .insert({
+      title,
+      description,
+      order: 0,
+      column_id,
+      board_id,
+    })
+    .select()
+    .single();
+
+  if (error) throw new Error(error.message);
+
+  if (subtasks.length) {
+    const postData = subtasks.map((subtask, i) => ({
+      title: subtask.title,
+      order: i,
+      column_id,
+      board_id,
+      parent_id: data.id,
+    }));
+
+    const { data: subtasksData, error } = await supabaseBrowserClient
+      .from("tasks")
+      .insert(postData);
+  }
+
+  return data;
+}
