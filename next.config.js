d916052374334c710b78f88379aca547da69e0f4
Anurag <dev.anuragkr@gmail.com>
2025-03-08 16:55:46 +0530

    added redirect

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
 
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/app",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
